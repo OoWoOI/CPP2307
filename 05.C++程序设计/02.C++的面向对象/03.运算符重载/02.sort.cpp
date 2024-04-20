e1f43005ebfb9eec45ed41e43931cc9f5d359a75
OoWoOI <2437767452@qq.com>
2024-04-20 23:43:53 +0800

    实现简易版的sort

--- 05.C++程序设计/02.C++的面向对象/03.运算符重载/02.sort.cpp
+++ 05.C++程序设计/02.C++的面向对象/03.运算符重载/02.sort.cpp
@@ -6,9 +6,110 @@
  ************************************************************************/
 
 #include<iostream>
+#include<algorithm>
+#include<functional>
 using namespace std;
+
+#define BEGIN(x) namespace x {
+#define END(x) }
+
+
+//sort基本使用
+BEGIN(test1)
+
+class CMP {
+public :
+    CMP() {}
+    bool operator()(const int &a, const int &b) {
+        return a < b;
+    }
+    ~CMP(){};
+private :
+
+};
+
+void output(int *first, int *last, string str) {
+    cout << str;
+    for (int *i = first; i != last; i++) {
+        cout << *i <<  " ";
+    }
+    cout << endl;
+    return ;
+}
+
+bool cmp1(const int &a, const int &b) {
+    return a < b;
+}
+
 int main() {
-    
+    int arr[100], n;
+    cin >> n;
+    for (int i = 0; i < n; i++) cin >> arr[i];
+    sort(arr, arr + n);
+    output(arr, arr + n, "none : ");
+    sort(arr, arr + n, cmp1);
+    output(arr, arr + n, "cmp1 : ");
+    CMP cmp2;
+    sort(arr, arr + n, cmp2);
+    output(arr, arr + n, "cmp2 : ");
+
+    return 0;
+}
+
+END(test1)
+
+//实现sort函数
+BEGIN(test2)
+
+class CMP {
+public :
+    CMP() {}
+    bool operator()(const int &a, const int &b) {
+        return a < b;
+    }
+    ~CMP(){};
+private :
+
+};
+
+void output(int *first, int *last, string str) {
+    cout << str;
+    for (int *i = first; i != last; i++) {
+        cout << *i <<  " ";
+    }
+    cout << endl;
+    return ;
+}
+
+bool cmp1(const int &a, const int &b) {
+    return a < b;
+}
+
+
+void sort(int *first, int *last, function<bool(int, int)> cmp = less<int>()) {
+    //TODO
+    return ;
+}
+
+int main() {
+    int arr[100], n;
+    cin >> n;
+    for (int i = 0; i < n; i++) cin >> arr[i];
+    sort(arr, arr + n);
+    output(arr, arr + n, "none : ");
+    sort(arr, arr + n, cmp1);
+    output(arr, arr + n, "cmp1 : ");
+    CMP cmp2;
+    sort(arr, arr + n, cmp2);
+    output(arr, arr + n, "cmp2 : ");
+
+    return 0;
+}
+
+END(test2)
+
+int main() {
+    test1::main();
     
     return 0;
 }
